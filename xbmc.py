cc4688e121595689dd5b9b94083d366e4793dd66
Kevin Mancuso <kevin@mancu.so>
2013-12-20 10:39:37 -0600

    fuck sqlalchemy

--- xbmc.py
+++ xbmc.py
@@ -1,120 +1,43 @@
 # -*- coding: utf-8 -*-
 
-import datetime
+#import datetime
 import os
+import MySQLdb.cursors
 
 from flask import Flask, render_template
-from flask.ext.sqlalchemy import SQLAlchemy
+from flaskext.mysql import MySQL
 from PIL import Image
 
-import sqlalchemy as sa
-from sqlalchemy.sql.expression import cast
 
 ## Configs
-db_conf = 'mysql://xbmc:xbmc@localhost/MyVideos75'
 thumbWebRoot = 'http://192.168.1.20/xbmc_thumbs'
 thumbRoot = '/mnt/data/xbmc/thumbs/'
 resizeThumbs = True
 resizedThumbRoot = '/var/www/xbmc_thumbs/'
+## DB Config
+db_host = 'localhost'
+db_user = 'xbmc'
+db_password = 'xbmc'
+db_name = 'MyVideos75'
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = db_conf
-db = SQLAlchemy(app)
+
+app.config['MYSQL_DATABASE_HOST'] = db_host
+app.config['MYSQL_DATABASE_USER'] = db_user
+app.config['MYSQL_DATABASE_PASSWORD'] = db_password
+app.config['MYSQL_DATABASE_DB'] = db_name
+mysql = MySQL()
+mysql.init_app(app)
 
 
-class NumericString(sa.types.TypeDecorator):
-    impl = sa.types.Numeric
-
-    def column_expression(self, col):
-        return cast(col, sa.Integer)
-
-
-class Tag(db.Model):
-    __tablename__ = 'tag'
-
-    id = db.Column('idTag', db.Integer, primary_key=True)
-    tag = db.Column('strTag', db.Text, unique=True)
-
-
-class TagLink(db.Model):
-    __tablename__ = 'taglinks'
-
-    idTag = db.Column('idTag', db.Integer, db.ForeignKey('tag.id'))
-    idMedia = db.Column('idMedia', db.Integer, primary_key=True)
-    mediaType = db.Column('media_type', db.Text)
-
-
-class Art(db.Model):
-    __tablename__ = 'art'
-
-    id = db.Column('art_id', db.Integer, primary_key=True)
-    media_id = db.Column('media_id', db.Integer)
-    media_type = db.Column('media_type', db.Text)
-    art_type = db.Column('type', db.Text)
-    url = db.Column('url', db.Text)
-
-
-class StreamDetails(db.Model):
-    __tablename__ = 'streamdetails'
-
-    idFile = db.Column('idFile', db.Integer, primary_key=True)
-    stream_type = db.Column('iStreamType', db.Integer)
-    video_codec = db.Column('strVideoCodec', db.Text)
-    video_aspect = db.Column('fVideoAspect', db.Float)
-    video_width = db.Column('iVideoWidth', db.Integer)
-    video_height = db.Column('iVideoHeight', db.Integer)
-    audio_codec = db.Column('strAudioCodec', db.Text)
-    audio_channels = db.Column('iAudioChannels', db.Integer)
-    audio_lang = db.Column('strAudioLanguage', db.Text)
-    subtitle_lang = db.Column('strSubtitleLanguage', db.Text)
-    video_duration = db.Column('iVideoDuration', db.Integer)
-
-
-class Movie(db.Model):
-    __tablename__ = 'movie'
-
-    id = db.Column('idMovie', db.Integer, primary_key=True)
-    title = db.Column('c00', db.Text)
-    plot = db.Column('c01', db.Text)
-    plot_outline = db.Column('c02', db.Text)
-    tagline = db.Column('c03', db.Text)
-    votes = db.Column('c04', db.Text)
-    rating = db.Column('c05', db.Text)
-    writers = db.Column('c06', db.Text)
-    year = db.Column('c07', db.Text)
-    thumbnails = db.Column('c08', db.Text)
-    imdb_id = db.Column('c09', db.Text)
-    sort_title = db.Column('c10', db.Text)
-    runtime = db.Column('c11', NumericString)
-    mpaa_rating = db.Column('c12', db.Text)
-    imdb_ranking = db.Column('c13', db.Text)
-    genre = db.Column('c14', db.Text)
-    director = db.Column('c15', db.Text)
-    orig_title = db.Column('c16', db.Text)
-    thumb_spoof = db.Column('c17', db.Text)
-    studio = db.Column('c18', db.Text)
-    trailer_url = db.Column('c19', db.Text)
-    fanart_urls = db.Column('c20', db.Text)
-    country = db.Column('c21', db.Text)
-    path = db.Column('c22', db.Text)
-    id_path = db.Column('c23', db.Text)
-    file_id = db.Column('idFile', db.Integer)
-    arts = db.relationship(Art, primaryjoin='Movie.id == Art.media_id',
-                           foreign_keys='Art.media_id')
-    stream = db.relationship(StreamDetails, primaryjoin='Movie.file_id =='
-                             ' StreamDetails.idFile',
-                             foreign_keys='StreamDetails.idFile')
-
-
+## Routes
 @app.route('/')
 def home():
     navContext = {'main': 'Home', 'bread': [['/', 'Home']]}
     mediaCount = {'tveps': 0, 'songs': 0}
-    res = db.session.query(sa.func.count(Movie.id),
-                           sa.func.sum(Movie.runtime)).first()
-    mediaCount['movies'] = res[0]
-    mediaCount['moviesRunTime'] = datetime.timedelta(0, res[1])
+    movies = getMovies()
+    mediaCount['movies'] = len(movies)
 
     return render_template('home.html', navContext=navContext,
                            mediaCount=mediaCount)
@@ -125,13 +48,16 @@ def movies():
     navContext = {'main': 'Movies', 'bread': [['/', 'Home'],
                                               ['/movies', 'Movies']]}
     info = {}
-    movies = Movie.query.order_by(Movie.id)
-    info['count'] = movies.count()
-    posters = {}
+    movies = getMovies()
+    movies = sorted(movies, key=lambda x: x['title'])
+    info['count'] = len(movies)
+    posters = getArt('WHERE type = "poster" AND media_type = "movie"')
     for item in movies:
-        posters[item.id] = getPoster(item.arts)
+        poster = [row for row in posters if row['media_id'] == item['id']]
+        if len(poster) > 0:
+            item['poster'] = getPoster(poster[0]['url'])
     return render_template('movies.html', navContext=navContext, movies=movies,
-                           info=info, posters=posters)
+                           info=info)
 
 
 @app.route('/movie/<moveIdx>')
@@ -153,18 +79,60 @@ def music():
     return render_template('music.html', navContext=navContext)
 
 
-def getPoster(art):
-    url = None
-    for item in art:
-        if item.art_type == 'poster':
-            url = item.url
-            break
-    if url:
-        fileHash = get_crc32(url)
-        poster = '%s/%s/%s.jpg' % (thumbWebRoot, fileHash[0], fileHash)
-    else:
-        poster = None
-    if resizeThumbs and url:
+## Database Functions
+def getMovies():
+    query = ('SELECT idMovie AS id, '
+             'c00 AS title, '
+             'c01 AS plot, '
+             'c02 AS plot_outline, '
+             'c03 AS tagline, '
+             'c04 AS votes, '
+             'c05 AS rating, '
+             'c06 AS writers, '
+             'c07 AS year, '
+             'c08 AS thumbnails, '
+             'c09 AS imdb_id, '
+             'c10 AS sort_title, '
+             'c11 AS runtime, '
+             'c12 AS mpaa_ranking, '
+             'c13 AS imdb_ranking, '
+             'c14 AS genre, '
+             'c15 AS director, '
+             'c16 AS orig_title, '
+             'c18 AS studio, '
+             'c19 AS trailer_url, '
+             'c20 AS fanart_url, '
+             'c21 AS country, '
+             'c22 AS path, '
+             'c23 AS id_path, '
+             'idFile from movie')
+    cursor = mysql.get_db().cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute(query)
+    movies = cursor.fetchall()
+
+    return movies
+
+
+def getArt(where=None):
+    query = ('SELECT art_id, '
+             'media_id, '
+             'media_type, '
+             'type, '
+             'url from art')
+    if where:
+        query = query + ' ' + where
+    cursor = mysql.get_db().cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute(query)
+    art = cursor.fetchall()
+
+    return art
+
+
+## Misc Junk Functions
+def getPoster(url):
+    fileHash = get_crc32(url)
+    poster = '%s/%s/%s.jpg' % (thumbWebRoot, fileHash[0], fileHash)
+    if resizeThumbs:
         gen_thumb(fileHash)
     return poster
 
@@ -174,7 +142,7 @@ def gen_thumb(image):
     thumbfile = '%s/%s/%s.jpg' % (resizedThumbRoot, image[0], image)
     exists = os.path.exists(thumbfile)
     if not exists:
-        #print "Making thumb for %s to %s" % (origfile, thumbfile)
+        print "Making thumb for %s to %s" % (origfile, thumbfile)
         mkthumb(origfile, thumbfile)
 
 
